df322e62bf9b179b51765402ac6192ae216e1e70
Eric Vicenti <ericvicenti@gmail.com>
2021-12-21 17:13:54 -0800

    Try expo-yarn-workspaces

--- apps/xpo/metro.config.js
+++ apps/xpo/metro.config.js
@@ -1,29 +1,3 @@
-const path = require('path');
-const getWorkspaces = require('get-yarn-workspaces');
-const workspaces = getWorkspaces(__dirname);
-const pkg = require("./package.json")
+const { createMetroConfiguration } = require('expo-yarn-workspaces');
 
-const pkgName = pkg.name
-
-module.exports = {
-  // projectRoot: path.resolve(__dirname, 'apps/rnative'),
-
-  watchFolders: [path.resolve(__dirname, 'node_modules'), ...workspaces],
-
-  resolver: {
-    blocklist: workspaces.map(
-      workspacePath =>
-        `/${workspacePath.replace(
-          /\//g,
-          '[/\\\\]',
-        )}[/\\\\]node_modules[/\\\\]react-native[/\\\\].*/`,
-    ),
-
-    extraNodeModules: {
-      'react-native': path.resolve(
-        __dirname,
-        `apps/${pkgName}/node_modules/react-native`,
-      ),
-    },
-  },
-};
+module.exports = createMetroConfiguration(__dirname);
